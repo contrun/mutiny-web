7ddf9892acfc11916c7babe38b42a3b797861c40
YI <uuuuuu@protonmail.com>
2024-05-01 17:10:41 +0800

    update nix flake

--- flake.nix
+++ flake.nix
@@ -6,20 +6,20 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+      let pkgs = import nixpkgs { inherit system; };
 
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.corepack_20
             pkgs.nodejs_20
+            pkgs.playwright
             pkgs.python3
             pkgs.just
           ];
+          PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+          PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
+
           shellHook = ''
             corepack prepare pnpm@8.15.5 --activate
           '';
